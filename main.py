927a6019267ff7b82199638eb57b1fe5bd72d9a1
Gianluca Benucci <gianlucabenucci9700@gmail.com>
2023-08-30 23:16:13 +0200

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
