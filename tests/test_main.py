07b565e36320bde5d1b3144b29ff5b895673bd63
Gianluca Benucci <gianlucabenucci9700@gmail.com>
2023-09-01 01:23:01 +0200

    test: add test for read_item route

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, FastAPI!"}
+
+
+def test_read_item():
+    response = client.get("/items/1997")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 1997}
