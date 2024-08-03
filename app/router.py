3998dc75586628b15d08339ad6cdcbfbd89ce623
Trusted97 <benux9700@gmail.com>
2024-08-03 10:58:33 +0200

    feat!: changed from poetry to rye"

--- app/router.py
+++ app/router.py
@@ -3,19 +3,15 @@ from fastapi import FastAPI
 app = FastAPI(
     title="FastAPI Boilerplate",
     description="A boilerplate project for FastAPI.",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 
 @app.get("/")
 def read_root():
-    return {
-        "message": "Hello, FastAPI!"
-    }
+    return {"message": "Hello, FastAPI!"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    return {
-        "item_id": item_id
-    }
+    return {"item_id": item_id}
